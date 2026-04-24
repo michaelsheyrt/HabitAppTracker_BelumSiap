a4968f28ca8370235d87d2f98688da21c673227d
michaelsheyrt <michaelsheyrt@gmail.com>
2026-04-24 15:47:21 +0700

    penyelesaian LoginFragment.kt dan setup DashboardFragment.kt, fragment_dashboard.xml dan implentasi navigation dari login ke dashboard

--- app/src/main/java/com/example/habitapptracker/view/LoginFragment.kt
+++ app/src/main/java/com/example/habitapptracker/view/LoginFragment.kt
@@ -1,7 +1,28 @@
-package com.example.habitapptracker.view
+package com.example.habitapp.view
 
+import android.os.Bundle
+import android.view.View
+import android.widget.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.habitapptracker.R
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val username = view.findViewById<EditText>(R.id.etUsername)
+        val password = view.findViewById<EditText>(R.id.etPassword)
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            if (username.text.toString() == "student" &&
+                password.text.toString() == "123"
+            ) {
+                findNavController().navigate(R.id.action_login_to_dashboard)
+            } else {
+                Toast.makeText(requireContext(), "Login gagal", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
